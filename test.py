3e0865495434b4805c6d9c4ad5abbac696b53c32
JeremyFriesenGitHub <jeremyfriesen@cmail.carleton.ca>
2025-02-03 19:26:17 -0500

    add A-LINE

--- test.py
+++ test.py
@@ -29,6 +29,9 @@ def djikstra(adjacency_list, start, end):
         current_vertex = previous[current_vertex]
     return path
 
+# A-LINE
+A = [[1],[0,2,3],[1,3,4],[1,2,4],[2,3,5],[4]]
+
 # B-LINE
 adjacency_list = [[1,2],[0,2,3,4],[0,1,3,4],[1,2,4,5],[1,2,3,5],[3,4,6,7],[5,7,8,9],[5,6,8,9],[6,7,9,10],[6,7,8,10],[8,9]]
 
